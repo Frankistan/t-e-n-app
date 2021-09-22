b4bcfb85be821220e8fe963482dddd56aa7ba8fc
Frankistan <omegasteel@hotmail.com>
2021-09-22 12:23:38 +0200

    #3

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -4,27 +4,22 @@ import jwt from "express-jwt";
 
 config();
 
+// fuente: 
+// https://www.youtube.com/watch?v=uSh5YRpqHog
+// https://www.youtube.com/watch?v=qVUr4YC6ZXA
+// https://javascript.plainenglish.io/creating-a-rest-api-with-jwt-authentication-and-role-based-authorization-using-typescript-fbfa3cab22a4
+
+
 export const AuthMiddleware = jwt({
     algorithms: ['HS256'],
     secret: process.env.JWT_SECRET,
     credentialsRequired: true,
+    userProperty: 'payload',
     getToken: (req: Request) => {
 
-        if (
-            req.headers.authorization &&
-            req.headers.authorization.split(" ")[0] === "Bearer"
-        ) {
-            // Authorization: Bearer xxxxxxxxxxxxxx
-            // Handle token presented as a Bearer token in the Authorization header
-            req.query.token = req.headers.authorization.split(" ")[1];
-            return req.headers.authorization.split(" ")[1];
-        } else if (req.query && req.query.token) {
-            // Handle token presented as URI param
-            return req.query.token;
-        } else if (req.cookies && req.cookies.token) {
-            // Handle token presented as a cookie parameter
-            return req.cookies.token;
-        }
+        let token = req.headers.authorization?.split(" ")[1] || req.query?.token || req.cookies?.token;
+
+        if (token) return token;
         // If we return null, we couldn't find a token.
         // In this case, the JWT middleware will return a 401 (unauthorized) to the client for this request
         return null;
